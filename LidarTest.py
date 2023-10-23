0ae75d480daec874275324b12817156a0d8e6bf5
Haider Iqbal <93882319+Luddo183@users.noreply.github.com>
2023-10-23 10:43:59 -0400

    Update LidarTest.py
    Better comments

--- LidarTest.py
+++ LidarTest.py
@@ -1,7 +1,7 @@
 from rplidar import RPLidar
 import matplotlib.pyplot as plt
 
-lidar = RPLidar("/dev/ttyUSB0")
+lidar = RPLidar("/dev/ttyUSB0") #Port at which lidar is connected
 
 fig = plt.figure(dpi=200)
 ax = fig.add_subplot(projection="polar")
@@ -16,16 +16,16 @@ for i, scan in enumerate(lidar.iter_scans()):
         if i == 0: #Change the number at which the loop breaks to do more or less scans
             break
 
+lidar.stop() #stopping lidar since we are no longer using it
+lidar.stop_motor()
+lidar.disconnect()
+
 for i in scans:
     q, a, d = i #q = quality of scan, a = angle, d = distance from origin
     angles.append(a)
     distances.append(d/10) #distance comes from the sensor in millimeters, change the value at which d is divided by to change unit
 
-lidar.stop()
-lidar.stop_motor()
-lidar.disconnect()
-
-c = ax.scatter(angles, distances, marker="o")
+c = ax.scatter(angles, distances, marker="o") # plotting values in polar graph
 ax.set_ylim(0, 40) #use this to constrain the graph to only a certain maximum y-axis value
 
-plt.savefig("lidarGraph.png", bbox_inches = "tight")
+plt.savefig("lidarGraph.png", bbox_inches = "tight") #saves graph as lidarGraph.png
